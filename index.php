2803bc6fd0f40f688d7c5928b50d79833332f010
Evina-Suanes24 <23-79023@g.batstate-u.edu.ph>
2026-05-02 15:08:28 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
 <?php
 echo "Hello CI/CD World!";
 ?>
-<-w /home/netadmin/cicd/prod_data.db -p war -k test --> <-w /home/netadmin/cicd/prod_data.db -p war -k test2 --> <-w /home/netadmin/cicd/prod_data.db -p war -k test --> <-w /home/netadmin/cicd/prod_data.db -p war -k auto trigger test --> <-w /home/netadmin/cicd/prod_data.db -p war -k new test --> 
