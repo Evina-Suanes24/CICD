c9c2ea88e40042d0d3028411b54099f7449dec02
Evina-Suanes24 <23-79023@g.batstate-u.edu.ph>
2026-05-02 08:12:48 +0800

    Test webhook trigger

--- index.php
+++ index.php
@@ -3,3 +3,4 @@ echo "<h1>Hello CI/CD World</h1>";
 ?>
 <-w /home/netadmin/cicd/prod_data.db -p war -k test -->
 <-w /home/netadmin/cicd/prod_data.db -p war -k test2 -->
+<-w /home/netadmin/cicd/prod_data.db -p war -k test -->
