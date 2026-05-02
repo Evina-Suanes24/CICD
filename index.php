463eeb96bee934dc026264a53ba940d2d2248bde
Evina-Suanes24 <23-79023@g.batstate-u.edu.ph>
2026-05-02 14:35:39 +0800

    Test auto trigger

--- index.php
+++ index.php
@@ -5,3 +5,4 @@ echo "Evina Cute hahahhaa";
 <-w /home/netadmin/cicd/prod_data.db -p war -k test2 -->
 <-w /home/netadmin/cicd/prod_data.db -p war -k test -->
 <-w /home/netadmin/cicd/prod_data.db -p war -k auto trigger test -->
+<-w /home/netadmin/cicd/prod_data.db -p war -k new test -->
